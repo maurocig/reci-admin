744e0b89b4b6b4c8c053e1b36b259ea105cca30e
Mauro Cigliuti <mcigliuti01@gmail.com>
2023-01-11 14:10:28 -0300

    deleted comments

--- src/public/js/formatDate.js
+++ src/public/js/formatDate.js
@@ -2,13 +2,11 @@ const moment = require('moment-timezone');
 
 module.exports = {
   formatDate: function (date) {
-    // return date ? new Date(date).toLocaleDateString('en-US') : '';
     formattedDate = moment(date).format('DD/MM/YYYY');
     return formattedDate;
   },
   formatDateValue: function (date) {
     formattedDate = moment(date).format('YYYY/MM/DD').replaceAll('/', '-');
-    console.log(formattedDate);
     return formattedDate;
   },
 };
